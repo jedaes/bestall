e1e664daad8fde250d8b5e566950606baf979e27
jedaes <75648250+jedaes@users.noreply.github.com>
2020-12-07 19:23:40 -0500

    Create firstpython.py
    Adding a new  file.

---
+++ firstpython.py
@@ -0,0 +1,3 @@
+Esta es una prueb de como agragar un archivo en GitHub.
+GitHub es muy Util en el campo de la ciencia de los datos.
+In order to better manage the data, We should learn to work with Hithub, so we can host this data in the Cloud.
